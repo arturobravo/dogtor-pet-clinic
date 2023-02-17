ff5f5a0c3d2f029ac7532382d684fe159f86ae32
Arturo Bravo <arthurspeedwarrior@gmail.com>
2023-02-16 21:30:44 -0600

    Initial project commit

---
+++ src/main/java/com/abb/dogtorpetclinic/DogtorPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.abb.dogtorpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogtorPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogtorPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abb/dogtorpetclinic/DogtorPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abb.dogtorpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogtorPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
